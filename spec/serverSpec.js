43f2d19a1786739f1f1788ff380359b57555a52a
Renan Oliveira <renan.oliveira_10@hotmail.com>
2018-03-02 19:04:26 -0300

    adicionando o jasmine ao projeto

---
+++ spec/serverSpec.js
@@ -0,0 +1,15 @@
+describe("Testes Para o servidor", function(){
+    
+    beforeEach(function(){
+        this.foo = 1;
+    });
+
+    it("Teste inicial", function() {
+        expect(this.foo).toEqual(1);
+    });
+
+    it("Teste inicial2", function() {
+        expect(this.foo).toEqual(1);
+    });
+
+})
